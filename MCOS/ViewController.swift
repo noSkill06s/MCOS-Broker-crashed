050e247263b630e940d2c662ef36fa3a88ffb685
Cankurt Burhan <burhan.cankurt@icloud.com>
2023-09-16 00:36:27 +0200

    Initial Commit

---
+++ MCOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MCOS
+//
+//  Created by Burhan Cankurt on 16.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
