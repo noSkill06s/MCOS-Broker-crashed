3d1f7248837be8bf5a36320b07fcf460a937c865
Cankurt Burhan <burhan.cankurt@icloud.com>
2023-09-17 03:21:29 +0200

    Finished: Pickerview

--- MCOS/MainController/HomeViewController.swift
+++ MCOS/MainController/HomeViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // Einbindung Pickerview
+    @IBOutlet var pickerview: UIPickerView!
+    
     // Instantiierung der Child-ViewControllers aus dem Storyboard
     let childDepotViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DepotViewController") as! DepotViewController
     let childWatchlistViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "WatchlistViewController") as! WatchlistViewController
@@ -17,6 +20,10 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Pickerview Initialisierung
+        pickerview.delegate = self
+        pickerview.dataSource = self
         
         // Schritt 1: Childs hinzufügen
         addChild(childDepotViewController)
@@ -50,5 +57,55 @@ class HomeViewController: UIViewController {
     }
 }
 
+// MARK: - UIPickerViewDataSource
+extension HomeViewController: UIPickerViewDataSource {
+    // Pickerview anzahl Spalten
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    // Pickverview anzahl Zeilen
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4  // Weil Sie 4 verschiedene Child-ViewControllers haben
+    }
+}
+
+// MARK: - UIPickerViewDelegate
+extension HomeViewController: UIPickerViewDelegate {
+    // Pickerview zeilen Namen
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let options = ["Depot", "Watchlist", "Chart", "TCR"]
+        return options[row]
+    }
+
+    // Pickerview ausgewählte Zeile
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Hier bestimmen Sie, welcher Child-ViewController angezeigt wird, basierend auf der ausgewählten Zeile.
+        switch row {
+        case 0:
+            childDepotViewController.view.isHidden = false
+            childWatchlistViewController.view.isHidden = true
+            childChartViewController.view.isHidden = true
+            childTCRViewController.view.isHidden = true
+        case 1:
+            childDepotViewController.view.isHidden = true
+            childWatchlistViewController.view.isHidden = false
+            childChartViewController.view.isHidden = true
+            childTCRViewController.view.isHidden = true
+        case 2:
+            childDepotViewController.view.isHidden = true
+            childWatchlistViewController.view.isHidden = true
+            childChartViewController.view.isHidden = false
+            childTCRViewController.view.isHidden = true
+        case 3:
+            childDepotViewController.view.isHidden = true
+            childWatchlistViewController.view.isHidden = true
+            childChartViewController.view.isHidden = true
+            childTCRViewController.view.isHidden = false
+        default:
+            break
+        }
+    }
+}
 
 
